fee57f77748a0bf4a745c02b8078bd37f1fd963b
tori4582 <hungtran.6360@gmail.com>
2021-09-10 00:01:50 +0700

    Inspecting Bean names

--- harurest/src/main/java/src/haru/harurest/HarurestApplication.java
+++ harurest/src/main/java/src/haru/harurest/HarurestApplication.java
@@ -1,7 +1,12 @@
 package src.haru.harurest;
 
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 /*
 	@SpringBootApplication:
@@ -17,6 +22,20 @@ public class HarurestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HarurestApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext context) {
+		return (args) -> {
+			System.out.println("Inspecting Beans provided by Spring Boot:");		
+
+			String[] beanNames = context.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+		};
 	}
 
 }
